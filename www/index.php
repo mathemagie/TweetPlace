0d27bafcbc8926ed7c9faf8265126848b5dadb8a
mathemagie <aurelien.fache@gmail.com>
2013-04-06 12:18:09 +0200

    first commit

---
+++ www/index.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Step 1: Require the Slim Framework
+ *
+ * If you are not using Composer, you need to require the
+ * Slim Framework and register its PSR-0 autoloader.
+ *
+ * If you are using Composer, you can skip this step.
+ */
+require 'Slim/Slim.php';
+require_once "pusher-php-server/lib/Pusher.php";
+$app_key = '6d6e276b32c15e5e131c';
+$app_secret = '2adf155d7261b759d446';
+$app_id = '34650';
+$pusher = new Pusher($app_key, $app_secret, $app_id);
+
+\Slim\Slim::registerAutoloader();
+
+/**
+ * Step 2: Instantiate a Slim application
+ *
+ * This example instantiates a Slim application using
+ * its default settings. However, you will usually configure
+ * your Slim application now by passing an associative array
+ * of setting names and values into the application constructor.
+ */
+$app = new \Slim\Slim();
+
+/**
+ * Step 3: Define the Slim application routes
+ *
+ * Here we define several Slim application routes that respond
+ * to appropriate HTTP request methods. In this example, the second
+ * argument for `Slim::get`, `Slim::post`, `Slim::put`, and `Slim::delete`
+ * is an anonymous function.
+ */
+
+// GET route
+$app->get('/', function () {
+    $template = <<<EOT
+API la-tapisserie.net 
+EOT;
+    echo $template;
+});
+
+$app->get('/status', function () {
+	$filename = "./status.txt";
+	$handle = fopen($filename, "r");
+	$contents = fread($handle, filesize($filename));
+	echo $contents;
+	fclose($handle);
+
+});
+
+// POST route
+$app->post('/s', function () {
+	global $pusher;
+	if (1) {
+		$fp = fopen('./status.txt', 'w+');
+		fwrite($fp, $_POST['status']);
+		fclose($fp);
+		 echo 'Set status for la tapisserie : ' . $_POST['status'];
+	  }
+});
+
+$app->run();
---
+++ www/lieu.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html><html>
+  <head>
+    <meta charset="utf-8">
+    <title>La Tapisserie</title>
+    <link href="lieu.css" rel="stylesheet">
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>
+      function initialize() {
+        var fenway = new google.maps.LatLng(48.858852,2.381448);
+        var panoOptions = {
+    			pov: {
+    			heading: 250,
+    			pitch: 1,
+    			zoom: -1
+    			},
+          position: fenway,
+          addressControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM
+          },
+          linksControl: false,
+          panControl: false,
+          zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL
+          },
+          enableCloseButton: false
+        };
+
+        var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('pano'), panoOptions);
+      }
+
+
+    </script>
+ 
+  <script src="http://js.pusher.com/1.12/pusher.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+
+	function get_tap_status() {
+	   $.ajax({
+	    url : '/status',
+	    success : function(data){
+		      if (data == 1) {
+				  $('#status_tapisserie').css('background','#33FF00');
+		    }else {
+				 $('#status_tapisserie').css('background','red');
+		     }
+
+	    } //votre callback
+	      });
+	}
+
+
+	function get_status() {
+		timeOut = setTimeout('get_status()', 5000);//It calls itself every 200ms
+		get_tap_status();
+	}
+
+	$(document).ready(function() {
+		get_status();
+	});
+
+
+
+  </script>
+  </head>
+  <body onload="initialize()">
+    <div id="pano_container">
+      <div id="pano" style="width: 100%; height: 700px"></div>
+    </div>
+    <div id="status_tapisserie"></div>
+  </body>
+</html>
+
