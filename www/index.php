e28ca61bc9a9a72d53f5909c7a14aa2ce9b8aa87
mathemagie <aurelien.fache@gmail.com>
2013-04-26 10:52:07 +0200

    rigt status

--- www/index.php
+++ www/index.php
@@ -1,40 +1,13 @@
 <?php
-/**
- * Step 1: Require the Slim Framework
- *
- * If you are not using Composer, you need to require the
- * Slim Framework and register its PSR-0 autoloader.
- *
- * If you are using Composer, you can skip this step.
- */
 require 'Slim/Slim.php';
 require 'pusher-php-server-master/lib/Pusher.php';
 
 \Slim\Slim::registerAutoloader();
-
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
 $app = new \Slim\Slim();
-
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, and `Slim::delete`
- * is an anonymous function.
- */
 
 $app_id = '41686';
 $app_key = '15b20d20432e58d9debf';
 $app_secret = '4c8b190a15aacda94b7c';
-
 $pusher = new Pusher( $app_key, $app_secret, $app_id );
 
 // GET route
@@ -46,12 +19,15 @@ EOT;
 });
 
 $app->get('/status', function () {
+	header("Access-Control-Allow-Origin: *");
 	$filename = "./status.txt";
 	$handle = fopen($filename, "r");
 	$contents = fread($handle, filesize($filename));
-	echo $contents;
-	fclose($handle);
+	$arr = array('open' => $contents);
+    echo json_encode($arr);
 
+	//echo $contents;
+	fclose($handle);
 });
 
 // POST route
