965e32f5fa5de827fed2ee6ba9d8af162d180e36
Fache Aurélien <aurelien.fache@gmail.com>
2014-10-01 17:08:14 +0200

    Create tweet_place_button_yun.ino

---
+++ arduino/tweet_place_button_yun.ino
@@ -0,0 +1,54 @@
+#include <Process.h> 
+#include <Bridge.h>
+ 
+int led = 13;
+#define BUTTON 7
+int tapisserieOpen;
+int start_val_bouton;
+int val_bouton = 0;
+
+void setup() 
+{
+        Bridge.begin();
+        pinMode(led, OUTPUT); 
+        Serial.begin(9600);
+        pinMode(BUTTON, INPUT);
+        start_val_bouton = digitalRead(BUTTON);
+}
+
+void OpenTapisserie() {
+  Process p;        // Create a process and call it "p"
+  p.runShellCommand("curl -d 'status=1&debug=0'  http://api.la-tapisserie.net/s");
+  //or use runShellCommandAsynchronously instead of runShellCommand
+}
+
+void closeTapisserie() {
+  Process p;        // Create a process and call it "p"
+  p.runShellCommand("curl -d 'status=0&debug=0'  http://api.la-tapisserie.net/s");
+}
+
+void loop() 
+{
+       val_bouton = digitalRead(BUTTON);
+        Serial.println(val_bouton);
+        if(val_bouton) {
+               if (tapisserieOpen) {
+                 closeTapisserie();
+                   Serial.println("CLOSE");
+                    tapisserieOpen = 0;
+                  delay(4000);
+                 }
+                digitalWrite(led, LOW);
+        }
+        else {
+              if (tapisserieOpen == 0) {
+                 OpenTapisserie(); 
+                
+                 Serial.println("OPEN");
+                 tapisserieOpen = 1;
+                  delay(4000);
+              }
+              digitalWrite(led,HIGH); 
+        }
+        delay(200);
+ }
